981865caef3c5dc6787f6c4fa3d9a3a9a38f2eee
Colton Myers <colton.myers@gmail.com>
2014-11-23 12:31:23 -0700

    Add echo to publish script

--- publish.sh
+++ publish.sh
@@ -5,6 +5,8 @@ CONTENT=/home/basepi/Dropbox/Blogs/blog.cmyers.net/content
 OUTPUT=/home/basepi/Dropbox/Blogs/blog.cmyers.net/output
 SETTINGS=/home/basepi/Dropbox/Blogs/blog.cmyers.net/publishconf.py
 
+echo 'Publishing blog.cmyers.net'
+
 rm -rf /home/basepi/Dropbox/Blogs/blog.cmyers.net/output/*
 $PELICAN $CONTENT -o $OUTPUT -s $SETTINGS || exit $?
 rsync -r --delete /home/basepi/Dropbox/Blogs/blog.cmyers.net/output/ /var/www/blog.cmyers.net
