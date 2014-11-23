c34bca35ab90f1b6476d0f3c31d983bdee67f648
Colton Myers <colton.myers@gmail.com>
2014-11-23 11:37:19 -0700

    Fix settings for blog.cmyers.net

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,15 +15,15 @@ EXTRA_PATH_METADATA = {
 }
 
 # chunk theme
-THEME = 'themes/pelican-chunk'
-SITESUBTITLE = 'because base ten is too boring'
+THEME = 'themes/pelican-theme-irfan'
+SITESUBTITLE = ''
 FOOTER_TEXT = 'Powered by <a href="http://getpelican.com">Pelican</a>'
 DISPLAY_CATEGORIES_ON_MENU = False
 SINGLE_AUTHOR = True
 MINT = False
-GOOGLE_ANALYTICS = 'UA-56998543-1'
+GOOGLE_ANALYTICS = 'UA-57037100-1'
 
-DISQUS_SITENAME = 'blog-basepi-net'
+DISQUS_SITENAME = 'blog-cmyers-net'
 
 YEAR_ARCHIVE_SAVE_AS = 'archives/{date:%Y}/index.html'
 
@@ -43,8 +43,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-        ('RSS', 'http://blog.basepi.net/rss.xml'),
-        ('Github', 'http://github.com/basepi'),
+        ('RSS', 'http://blog.cmyers.net/rss.xml'),
         ('Twitter', 'http://twitter.com/basepi'),
 )
 
